aed64c0b41ac3c622ce2d5a8300c38730c519286
BrandonGS <brandongs180@gmail.com>
2022-10-25 02:18:59 -0500

    add components to barrel

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,6 @@
+export { default as ImageCard } from "./ImageCard/ImageCard";
+export { default as ImageCardSkeleton } from "./ImageCard/ImageCardSkeleton";
+export { default as ImageCardList } from "./ImageCardList/ImageCardList";
+export { default as ImageCardListSkeleton } from "./ImageCardList/ImageCardListSkeleton";
 export { default as InputSearch } from "./InputSearch/InputSearch";
 export { default as Link } from "./Link";
