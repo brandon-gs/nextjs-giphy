0de85339ab5b6c18b801d67e1c8f014d079ce1ef
BrandonGS <brandongs180@gmail.com>
2022-10-25 01:28:32 -0500

    modify limit and update types

--- src/services/giphy.ts
+++ src/services/giphy.ts
@@ -1,11 +1,15 @@
-const LIMIT = 5; // Limit results by query
+import type { GiphyTrendingResponse } from "@/types/Giphy";
+
+const LIMIT = 12; // Limit results by query
 
 interface GetTrendingRequest {
   pageParam: number;
 }
 
 export const GiphyService = {
-  async getTrending({ pageParam = 0 }: GetTrendingRequest) {
+  async getTrending({
+    pageParam = 0,
+  }: GetTrendingRequest): Promise<GiphyTrendingResponse> {
     const response = await fetch(
       `api/giphy/trending?limit=${LIMIT}&offset=${pageParam * LIMIT}`
     );
