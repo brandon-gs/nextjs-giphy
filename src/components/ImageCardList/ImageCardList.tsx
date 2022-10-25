b57948f72234df65ae612c4221ed3ee7ae6fb69e
BrandonGS <brandongs180@gmail.com>
2022-10-25 02:18:25 -0500

    refactor image card list component

---
+++ src/components/ImageCardList/ImageCardList.tsx
@@ -0,0 +1,64 @@
+import { FC, Fragment } from "react";
+import type { GiphyData, GiphyTrendingResponse } from "@/types/Giphy";
+import ImageCard from "../ImageCard/ImageCard";
+import { Stack } from "@mui/material";
+import ImageCardListSkeleton from "./ImageCardListSkeleton";
+import styles from "./ImageCardList.module.scss";
+import { InfiniteData } from "@tanstack/react-query";
+
+interface ImageCardListProps {
+  status: "loading" | "success" | "error";
+  isFetching: boolean;
+  isFetchingNextPage: boolean;
+  data?: InfiniteData<GiphyTrendingResponse>;
+  hasNextPage?: boolean;
+  error: unknown;
+  observerRef: (node: HTMLDivElement) => void;
+}
+
+const ImageCardList: FC<ImageCardListProps> = ({
+  status,
+  isFetching,
+  isFetchingNextPage,
+  data,
+  error,
+  hasNextPage,
+  observerRef,
+}) => {
+  const isError = error !== null;
+  const isLoadingFirstPage = isFetching && !isFetchingNextPage;
+
+  if (status === "loading" && isLoadingFirstPage) {
+    return <ImageCardListSkeleton />;
+  }
+
+  if (data === undefined || isError) {
+    return <p>Retry get data</p>;
+  }
+
+  return (
+    <Fragment>
+      <Stack className={styles.ImageCardList}>
+        {data.pages.map((group, idx) => (
+          <Fragment key={`group-trending-${idx}`}>
+            {group.data.map(({ id, images }, index) => (
+              <ImageCard
+                key={`image-${id}-idx-${index}`}
+                imageUrl={images.original.webp}
+                placeholderUrl={images.preview_webp.url}
+              />
+            ))}
+          </Fragment>
+        ))}
+      </Stack>
+      {hasNextPage && (
+        <>
+          <ImageCardListSkeleton />
+          <div ref={observerRef} />
+        </>
+      )}
+    </Fragment>
+  );
+};
+
+export default ImageCardList;
