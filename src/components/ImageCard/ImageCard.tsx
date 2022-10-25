1dca26423e69f1892208ebd326f72fcadfa3f9e4
BrandonGS <brandongs180@gmail.com>
2022-10-25 00:35:47 -0500

    add ImageCard component

---
+++ src/components/ImageCard/ImageCard.tsx
@@ -0,0 +1,28 @@
+import { FavoriteBorder } from "@mui/icons-material";
+import { Box, IconButton } from "@mui/material";
+import Image from "next/image";
+import { FC } from "react";
+import styles from "./ImageCard.module.scss";
+
+interface ImageCardProps {
+  imageUrl: string;
+  placeholderUrl: string;
+}
+
+const ImageCard: FC<ImageCardProps> = ({ imageUrl, placeholderUrl }) => {
+  return (
+    <Box className={styles.ImageCard}>
+      <Image
+        layout="fill"
+        objectFit="cover"
+        src={imageUrl}
+        placeholder={"blur"}
+        blurDataURL={placeholderUrl}
+      />
+      <IconButton className={styles.ButtonLike}>
+        <FavoriteBorder className={styles.LikeIcon} />
+      </IconButton>
+    </Box>
+  );
+};
+export default ImageCard;
